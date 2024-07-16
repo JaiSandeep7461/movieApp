47ab2655692b1c84c4dfe97e826a5725af7437e0
JaiSandeep7461 <jaisandeep7461@gmail.com>
2024-07-16 22:36:59 +0530

    Recycler View Item Click Listener is Added

--- app/src/main/java/com/example/movieapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/dashboard/DashboardFragment.kt
@@ -1,6 +1,7 @@
 package com.example.movieapp.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,6 +33,9 @@ class DashboardFragment : Fragment() {
     ): View? {
         _binding= FragmentDashboardBinding.inflate(inflater,container,false)
         popularAdapter = MoviesAdapter()
+        popularAdapter.onItemClick= {item->
+            Log.e("16072024","The Item is >>> ${item.title}")
+        }
         return binding.root
     }
 
--- app/src/main/java/com/example/movieapp/ui/dashboard/MoviesAdapter.kt
+++ app/src/main/java/com/example/movieapp/ui/dashboard/MoviesAdapter.kt
@@ -11,6 +11,8 @@ import com.example.movieapp.databinding.ItemTodoBinding
 
 class MoviesAdapter : PagingDataAdapter<Item, MoviesAdapter.MovieViewHolder>(DiffCallback()) {
 
+    var onItemClick: ((Item) -> Unit)? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val binding = ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MovieViewHolder(binding)
@@ -19,17 +21,20 @@ class MoviesAdapter : PagingDataAdapter<Item, MoviesAdapter.MovieViewHolder>(Dif
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = getItem(position)
         if (movie != null) {
-            holder.bind(movie)
+            holder.bind(movie,onItemClick)
         }
     }
 
     class MovieViewHolder(private val binding: ItemTodoBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: Item) {
+        fun bind(item: Item, onItemClick: ((Item) -> Unit)?) {
             binding.tvTitle.text = item.title
             Glide.with(binding.ivMovieImage.context)
                 .load("https://image.tmdb.org/t/p/w500${item.backdropPath}")
                 .into(binding.ivMovieImage)
+            binding.llMain.setOnClickListener {
+                onItemClick?.invoke(item)
+            }
         }
     }
 
